d45c03d7487d3dfdc2b1279980521d167ed91a47
Ms Katsiaryna Dalidovich <kdalid01@dcs.bbk.ac.uk>
2024-10-09 19:25:03 +0100

    testing loop branch

---
+++ RPS.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RPS {
+
+	public static void main (String[] arg) {
+	
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter 'R'ock, 'P'aper, 'S'icisors' > ");
+
+	String line = input.nextLine().toUpperCase();
+
+}
+
+public static boolean is Valid(String s) {
+	if (s.length() !=2) {
+	
+
+	char c1 = s.charAt(0);
+	char c2 = s.charAT(1);
+	
+	if (!isValid(s.charAt)
+	return true;
+}
